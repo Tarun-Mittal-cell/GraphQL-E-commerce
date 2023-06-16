e4a483c360b14eba162a9062d7047a3c17b9b4d4
Tarun Mittal <tm6622@g.rit.edu>
2023-06-15 21:50:44 -0400

    added separate files for products and orders for separation of concerns w.r.t. schema

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
 const express = require('express');
-const { buildSchema } = require('graphql');
+
 const { graphqlHTTP } = require('express-graphql');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
-const schema = buildSchema(`
-    type Query {
-        products: [Product]
-        orders: [Order]
-    }
 
-    type Product {
-        id: ID!
-        description: String!
-        reviews: [Review]
-        price: Float!
-    }
+const schema = makeExecutableSchema({
+    typeDefs: [schemaText]
+});
 
-    type Review {
-        rating: Int!
-        comment: String
-    }
-
-    type Order {
-        date: String!
-        subTotal: Float!
-        items: [OrderItem]
-    }
-
-    type OrderItem {
-        product: Product!
-        quantity: Int!
-    }
-`);
 
 const root = {
     products: [
